474a7a35f9c897df479f22d6e8f831b0859ca1a2
sasanqc <sasanqc@gmail.com>
2023-06-25 12:50:15 +0330

    added app title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="">
-      <Head />
+      <Head title="Kanban Web App" />
 
       <body className="font-jakarta">
         <Main />
